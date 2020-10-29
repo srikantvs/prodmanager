44982b30011190612e1c9ce690b1be452f6f7896
srikant <srikantvs26@gmail.com>
2020-10-29 18:49:18 +0530

    changes to code coverage config

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('OnlineDietSystem app is running!');
+    expect(page.getTitleText()).toEqual('ProdManager app is running!');
   });
 
   afterEach(async () => {
