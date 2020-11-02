72f67f4521ff4801e6bb95226bb8e85ce62cd4d7
srikant <srikantvs26@gmail.com>
2020-11-02 10:55:06 +0530

    added 2 analytics component testcases

--- src/app/analytics/analytics.component.spec.ts
+++ src/app/analytics/analytics.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AnalyticsComponent } from './analytics.component';
 import { ChartsModule, ThemeService } from 'ng2-charts';
+import { FormsModule } from '@angular/forms';
 
 describe('AnalyticsComponent', () => {
   let component: AnalyticsComponent;
@@ -9,7 +10,7 @@ describe('AnalyticsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports:[ChartsModule],
+      imports:[ChartsModule,FormsModule],
       declarations: [AnalyticsComponent],
       providers:[ThemeService]
     })
@@ -34,12 +35,43 @@ describe('AnalyticsComponent', () => {
     expect(component.pieChartLegend).toBeTruthy();
   });
 
-  it('check if top products are rendered', () => {
+  it('check if top 3 products are rendered', () => {
     component.pieChartData3 = [1,2];
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
     
     expect(compiled.querySelector('#top3').textContent).toContain('Top 3 Products');
   });
+
+  it('check if product analytics are rendered', () => {
+    component.pieChartData3 = [1, 2, 3, 4, 5];
+    component.eachProdCheck = true;
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    
+    expect(compiled.querySelector('#topx').textContent).toContain('Product Analytics');
+  });
+
+
+  it('top 3 products shoud be shown', () => {
+    component.pieChartData3 = [1,2,3,4];
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(component.istop3checked).toBeTruthy();
+    component.istop3checked = false;
+    fixture.detectChanges();
+    expect(component.istop3checked).toBeFalsy();
+  });
+
+  it('product analytics shoud be shown', () => {
+    component.pieChartData3 = [1,2,3,4,5];
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(component.eachProdCheck).toBeFalsy();
+     component.eachProdCheck = true;
+    fixture.detectChanges();
+     expect(component.eachProdCheck).toBeTruthy();
+  });
+
 
 });
